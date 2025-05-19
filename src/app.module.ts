11b2fafe56eec4d4fda7c6acd7fd0dbac87f3dc4
Alejandro Méndez <alejmendez.87@gmail.com>
2025-05-19 01:28:17 -0400

    Eliminación de archivos de controlador y servicio de la aplicación, así como ajustes en el módulo principal para eliminar referencias a estos componentes. Se mejora la organización de las importaciones en app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { DatabaseModule } from './modules/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { ProductsModule } from './modules/products/products.module';
 import { InventarioModule } from './modules/inventario/inventario.module';
-import { OrdersController } from './modules/orders/controllers/orders.controller';
 import { OrdersModule } from './modules/orders/orders.module';
 import { TablesModule } from './modules/tables/tables.module';
 import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule, CategoriesModule, ProductsModule, InventarioModule, OrdersModule, TablesModule, ClientsModule],
-  controllers: [AppController, OrdersController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    CategoriesModule,
+    ProductsModule,
+    InventarioModule,
+    OrdersModule,
+    TablesModule,
+    ClientsModule,
+  ],
 })
 export class AppModule {}
