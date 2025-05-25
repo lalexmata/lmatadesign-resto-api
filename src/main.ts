f2dc91e729575034ea5b69decf5b3048e3ddf296
Alejandro Méndez <alejmendez.87@gmail.com>
2025-05-25 14:25:54 -0400

    Se añaden las dependencias '@fastify/static' y '@nestjs/platform-fastify' en package.json y package-lock.json. Se refactoriza la inicialización de la aplicación para utilizar Fastify como adaptador, mejorando el rendimiento y la compatibilidad con el framework.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Lmatadesign Resto API')
