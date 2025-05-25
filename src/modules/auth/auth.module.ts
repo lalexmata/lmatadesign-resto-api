340bdd9e041c9332b4932fb6b7c5ae03aa09d098
Alejandro Méndez <alejmendez.87@gmail.com>
2025-05-25 03:23:37 -0400

    Se refactoriza la configuración del módulo de autenticación para utilizar la carga asíncrona de la configuración del JWT mediante el ConfigService, mejorando la gestión de secretos y la flexibilidad de la configuración.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
 import { UsersModule } from '@users/users.module';
 
-import { AuthService } from '@auth/services/auth.service';
 import { AuthController } from '@auth/controllers/auth.controller';
+import { AuthService } from '@auth/services/auth.service';
 import { JwtStrategy } from '@auth/strategy/jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>('JWT_SECRET');
+        return {
+          secret,
+          signOptions: { expiresIn: '1h' },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, JwtStrategy],
